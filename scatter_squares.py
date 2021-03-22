64ac9649e14ac0c9e22da9836bbbfbdbd2eadba7
Edward Cheadle <edward.cheadle8@gmail.com>
2021-03-22 13:01:12 -0600

    Add multiple points

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+plt.scatter(x_values, y_values)
 
 # Set chart title and label axis
 plt.title("Square Numbers", fontsize=24)

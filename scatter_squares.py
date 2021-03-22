965dd7c5a3cb9a99c8b653a8a32d291a963f628f
Edward Cheadle <edward.cheadle8@gmail.com>
2021-03-22 13:07:38 -0600

    Automatically generate data points.

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, s=40)
 
 # Set chart title and label axis
 plt.title("Square Numbers", fontsize=24)
@@ -12,4 +12,6 @@ plt.ylabel("Square of Value", fontsize=14)
 # Set size of tick labels.
 plt.tick_params(axis='both', labelsize=14)
 
+# Set the range for each axis.
+plt.axis([0, 1100, 0, 1100000])
 plt.show()
